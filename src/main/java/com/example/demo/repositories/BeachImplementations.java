684f69ef76d47fc28b41016297a895e50ad9dc34
Alvaro Cortes <cortesalvaro01@gmail.com>
2022-04-14 15:45:05 +0200

    fixed validations and more

--- src/main/java/com/example/demo/repositories/BeachImplementations.java
+++ src/main/java/com/example/demo/repositories/BeachImplementations.java
@@ -1,2 +1,25 @@
-package com.example.demo.repositories;public class BeachImplementations {
+package com.example.demo.repositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+
+@Repository
+@Transactional
+public class BeachImplementations implements BeachRepository {
+
+    @Override
+    public List retrieveSwellConditions(String id) {
+        String url = "http://services.surfline.com/kbyg/spots/forecasts/wave?spotId=" + id + "&days=6&intervalHours=1";
+        RestTemplate restTemplate = new RestTemplate();
+        LinkedHashMap result = ((LinkedHashMap) restTemplate.getForObject(url, Object.class));
+        //String result = restTemplate.getForObject(url, String.class);
+
+        List swell = new ArrayList();
+        swell.add(result);
+
+        return swell;
+    }
 }
--- src/main/java/com/example/demo/repositories/BeachRepository.java
+++ src/main/java/com/example/demo/repositories/BeachRepository.java
@@ -1,2 +1,13 @@
-package com.example.demo.repositories;public interface BeachRepository {
+package com.example.demo.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@Repository
+public interface BeachRepository {
+
+    List retrieveSwellConditions(String id);
 }
